fd0270aa2f57ebd383ffdd213bf9f8f43b53d936
BrenoDMoura <brenobatista1990@gmail.com>
2023-03-18 23:39:44 -0300

    Arquivo do URI Academic para a OBI

---
+++ URI Academic/FASE 1/URI_2448_Correios.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+	int n, m, comparador, cont = 0, posicaon, posicaom, tempoi = 0, tempof = 0, tempototal = 0;
+	
+	scanf("%d %d", &n, &m);
+	
+	int a[n], b[m];
+	
+	for (int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+	}
+	
+	for (int i = 0; i < m; i++){
+		scanf("%d", &b[i]);
+	}
+	
+	for(int i = 0; i < m; i++){
+		comparador = b[i];
+		
+		for (int i = 0; i < n; i++){
+			if(a[i] != comparador){
+				cont++;
+				
+			}
+		}
+	}
+	
+	printf("%d\n", cont/2);
+		
+		
+	
+	
+	return 0;
+}
