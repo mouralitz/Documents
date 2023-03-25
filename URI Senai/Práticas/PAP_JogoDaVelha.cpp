f42f8ed13bee8d5bc98cfecd8641a472c4859ede
BrenoDMoura <brenobatista1990@gmail.com>
2023-03-25 00:16:39 -0300

    Atualização feita no algoritmo.

--- URI Senai/Práticas/PAP_JogoDaVelha.cpp
+++ URI Senai/Práticas/PAP_JogoDaVelha.cpp
@@ -6,52 +6,62 @@ int main(void){
 	setlocale(LC_ALL, "Portuguese_Brazil");
 	
 	int tam;
-	int x, y;
-	char nome1[30], nome2[30], simbolo1, simbolo2;
+	int linha, coluna, nJogadores;
 	
-	printf("Informe o nome do jogador 1: \n");
-	scanf(" %[^\n]s", &nome1);
-	printf("%s, qual símbolo você gostaria de utilizar? \n", nome1);
-	scanf(" %c", &simbolo1);
-
 	
-	printf("Informe o nome do jogador 2: \n");
-	scanf(" %[^\n]s", &nome2);
-	printf("%s, qual símbolo você gostaria de utilizar? \n", nome2);
-	scanf(" %c", &simbolo2);
+	//Preenchendo informações a respeito dos jogadores.
+	
+	printf("Informe a quantidade de jogadores:\n");
+	scanf(" %d", &nJogadores);
+	
+	char nomes[nJogadores][100];
+	char simbolos[nJogadores];
+	
+	for(int i = 0; i < nJogadores; i++){
+	printf("Informe o nome do jogador %d: \n", i + 1);
+	scanf(" %[^\n]s", nomes[i]);
+	printf("%s, qual símbolo você gostaria de utilizar? \n", nomes[i]);                                                                                             
+	scanf(" %c", &simbolos[i]);
+	}	//.
+	
 	
 	//Definindo tamanho do tabuleiro.
 	printf("Qual o tamanho do tabuleiro, que desejam jogar? \n");
 	scanf(" %d", &tam);
 	
-	char tabuleiro[tam][tam];
+	char tabuleiro[tam][tam];	//.
 	
 	//Anulando deslocamentos da matriz.
 	for(int i = 0; i < tam; i++){
 		for (int j = 0; j < tam; j++){
 			tabuleiro[i][j] = ' ';
 		}
-	}
+	}	//.
 	
 	system("pause");
 	system("cls");
 		
 	printf("*** J O G O  D A  V E L H A ***\n");
 	printf("*******************************\n");
-	printf("Nome do Jogador 1: %s\n", nome1);
-	printf("Nome do Jogador 2: %s\n\n", nome2);
-	printf("Boa sorte jogadores %s e %s!\n", nome1, nome2);
+	for (int i = 0; i < nJogadores; i++){
+	printf("Nome do Jogador %d: %s\n", i+1, nomes[i]);	
+	}
+	printf("Boa sorte jogadores ");
+	for (int i = 0; i < nJogadores - 1; i++){
+		printf("%s e ", nomes[i]);
+	}
+	printf("%s!!!\n", nomes[nJogadores - 1]);
 	printf("*******************************\n");
 	
-
+	//Printando o tabuleiro.
 	for(int i = 0; i < tam*tam; i++){
-		printf(" ");
+		printf(" "); // i = max de jogadas para o tamanho do tabuleiro escolhido. 
 		for(int a = 0; a < tam; a++)
-		printf("  %d  ", a);
+		printf("  %d  ", a); // a = número das posições referentes as colunas.
 		printf("\n");
 		for(int j = 0; j < tam; j++){
-			printf("%d", j);
-			for(int k = 0; k < tam-1; k++){
+			printf("%d", j); // int j =  numero das posições referentes as linhas.
+			for(int k = 0; k < tam; k++){
 				printf("  %c |", tabuleiro[j][k]);
 			}
 			printf("\n  ");
@@ -61,18 +71,18 @@ int main(void){
 			if(j < tam-1)
 			printf("---");
 			printf("\n");
-		}
+		}	//.
+		
+		//Preenchedo tabuleiro.
 	printf("Digite as coordenadas\n");
-	scanf(" %d %d",&x, &y);
-		if(i % 2 == 0){
-		tabuleiro[x][y] = simbolo1;
-	}
-		else{
-		tabuleiro[x][y] = simbolo2;
-	}
+	scanf(" %d %d",&linha, &coluna);
+		
+		tabuleiro[linha][coluna] = simbolos[i % nJogadores];
+		
+	system("cls");
 	
 	
-	}
+	}	//.
 	
 	return 0;
 }
